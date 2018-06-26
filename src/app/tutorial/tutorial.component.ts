ca8f4489676cb3367d26c34887cfe448853b708e
TanMyBlog <kd.aigapa@gmail.com>
2018-06-26 18:34:03 +0700

    Data Biding Basic

--- src/app/tutorial/tutorial.component.ts
+++ src/app/tutorial/tutorial.component.ts
@@ -2,19 +2,10 @@ import { Component } from '@angular/core';
 
 @Component ({
     selector: 'my-tutorial',
-    template: `<h2>this is tutorial component</h2>
-        <h3>Hello Angular 6</h3>
-        <h4>Hello Angular 6</h4>
-        <h5>Hello Angular 6</h5>
-    `,
-    styles: [`
-        h2{color: red;} 
-        h3 {color: blue;}
-        h4 {color: yellow;}
-        h5 {color: silver}
-    `]
+    template: `<h2>this is tutorial component</h2> <img [src]="img">`,
+    styles: [`h2{color: red;}`]
 })
 
 export class TutorialComponent {
-    
+    public img = "http://lorempixel.com/300/300";
 }

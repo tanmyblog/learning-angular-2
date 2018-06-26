f21c7bc9f79f94fc6bf22acb589ea8b6880c9ef6
TanMyBlog <kd.aigapa@gmail.com>
2018-06-26 18:09:33 +0700

    Styling for component

--- src/app/tutorial/tutorial.component.ts
+++ src/app/tutorial/tutorial.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component ({
-    selector: "my-tutorial",
-    template: "<h2>this is tutorial component</h2>"
+    selector: 'my-tutorial',
+    template: `<h2>this is tutorial component</h2>
+        <h3>Hello Angular 6</h3>
+        <h4>Hello Angular 6</h4>
+        <h5>Hello Angular 6</h5>
+    `,
+    styles: [`
+        h2{color: red;} 
+        h3 {color: blue;}
+        h4 {color: yellow;}
+        h5 {color: silver}
+    `]
 })
 
 export class TutorialComponent {

91a6cc74817186c3d89e7f0027d07f847762f15e
TanMyBlog <kd.aigapa@gmail.com>
2018-06-26 18:42:31 +0700

    Binding class and Style for element

--- src/app/tutorial/tutorial.component.ts
+++ src/app/tutorial/tutorial.component.ts
@@ -2,10 +2,17 @@ import { Component } from '@angular/core';
 
 @Component ({
     selector: 'my-tutorial',
-    template: `<h2>this is tutorial component</h2> <img [src]="img">`,
-    styles: [`h2{color: red;}`]
+    template: `
+        <h3 [class.redColor]="applyClass">red color</h3>
+        <h4 [style.color]="blueColor?'blue':'orange'">Style Color</h4>
+    `,
+    styles: [`
+        .redColor {color: red;}
+        .color {color: blue}
+    `]
 })
 
 export class TutorialComponent {
-    public img = "http://lorempixel.com/300/300";
+    public applyClass = true;
+    public blueColor = false;
 }

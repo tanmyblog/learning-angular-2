ebc840d24be57734a0756ebe54120231492f74b4
TanMyBlog <kd.aigapa@gmail.com>
2018-06-27 21:22:17 +0700

    Form Validate part 1

--- src/app/tutorial/tutorial.component.ts
+++ src/app/tutorial/tutorial.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-tutorial',
-    templateUrl: 'tutorial.component.html',
-    styles: [``]
+    templateUrl: './tutorial.component.html',
+    styleUrls: ['./tutorial.component.css']
 })
 
 export class TutorialComponent {
